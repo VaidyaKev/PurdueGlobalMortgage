6d6df050fb2934789723914f0f4f8233c55c0f99
Kevin Vaidya <kevinv1990@gmail.com>
2024-05-01 22:16:13 -0400

    added ui skeleton

---
+++ src/UI/index.js
@@ -0,0 +1,8 @@
+const calculateBtn = document.getElementById('calculateBtn');
+
+calculateBtn.addEventListener('click', function() {
+    // Placeholder - Add logic to either:
+    // 1. Open a mortgage calculator form/popup
+    // 2. Redirect to a dedicated calculation page
+    alert('Mortgage Calculator - Coming Soon!'); 
+});
